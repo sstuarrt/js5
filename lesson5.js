60714aa4c94f551198e98f8894161d3f88e71063
sstuarrt <95904642+sstuarrt@users.noreply.github.com>
2022-10-22 18:51:10 +0300

    Add files via upload

---
+++ lesson5.js
@@ -0,0 +1,26 @@
+function makeBuffer() {
+    var text = '';
+
+    return function(piece) {
+        if (arguments.length == 0) {
+            return text;
+        }
+        text += piece;
+    };
+};
+
+var buffer = makeBuffer();
+
+buffer('Java Script ');
+buffer('Need to be ');
+buffer('Learned!');
+
+console.log(buffer());
+
+var buffer = makeBuffer();
+
+buffer(0);
+buffer(1);
+buffer(3);
+
+console.log(buffer());
